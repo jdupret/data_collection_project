3e45393a6729e0bfed6829c3e3fac1167d2c469d
Boubacar Traoré <boubatraore1980@gmail.com>
2024-02-08 17:46:10 +0000

    Menu latérale et divers

--- main.py
+++ main.py
@@ -3,28 +3,37 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 from menu import menu
 
+# Page Principale
 st.title("Projet de Data Collection Groupe 5")
-st.sidebar.title("Menu")
+st.write(
+    "This app performs webscraping of data from expat-dakar over multiples pages. "
+    "And we can also download scraped data from the app directly without scraping them.")
+st.markdown("- Python libraries: base64, pandas, streamlit, requests, bs4")
+st.markdown("- Data source: at-Dakar.")
 
+# Barre de Menu lattérale
+st.sidebar.title("User Input Features")
+
+list1 = [i for i in range(1, 101)]
 option1 = st.sidebar.selectbox(
-    "Qu'est-ce vous voulez scrapper ?",
-    ("Appartements à louer", "Meubles pour appartement", "Terrains à vendre"),
-    index=None,
-    placeholder="Sélectionner une option",
+    "Pages indexes",
+    list1,
+)
+
+option2 = st.sidebar.selectbox(
+    "Options",
+    ("Scrape data using beautifulSoup", "Download scraped data", "Dashbord of the data", "Fill the form")
 )
 
 st.write('Vous avez sélectionné:', option1)
-
-list1 = [i for i in range(1, 101)]
-option2 = st.sidebar.selectbox(
-    "Nombre de page à scrapper",
-    list1,
-    index=None,
-    placeholder="Sélectionner une option",
-)
-
 st.write('Vous avez sélectionné:', option2)
 
+st.sidebar.button("Scraper les Data", type="primary", on_click='scraper_page', args=(option1, option2))
 
-def scrapper():
+
+def scraper_page():
+    """
+    Fonction qui permet de scraper de les les pages avec BeautifulSoup
+    :return: Pandas DataFrame
+    """
     pass
