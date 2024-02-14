bfe78b73b24fee3533f28ae8d33e16056231960b
boubatracool <111995075+boubatracool@users.noreply.github.com>
2024-02-14 17:47:09 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -3,37 +3,10 @@ import base64
 import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup as bs
+from appart_a_louer import scrap_appart_a_louer
+from appart_meuble import scrap_appart_meuble
+from terrain_a_vendre import srappe_terrain_a_vendre
 
-# Page Principale
-st.title("Projet de Data Collection Groupe 5")
-st.write(
-    "This app performs webscraping of data from expat-dakar over multiples pages. "
-    "And we can also download scraped data from the app directly without scraping them.")
-st.markdown("- Python libraries: base64, pandas, streamlit, requests, bs4")
-st.markdown("- Data source: at-Dakar.")
-
-# Barre de Menu lattérale
-st.sidebar.title("User Input Features")
-
-# création d'une liste de 1 à 100
-list1 = [i for i in range(1, 101)]
-
-option1 = st.sidebar.selectbox(
-    "Pages indexes",
-    list1,
-)
-
-option2 = st.sidebar.selectbox(
-    "Options",
-    ("Scrape data using beautifulSoup", "Download scraped data", "Dashbord of the data", "Fill the form")
-)
-
-appart_a_louer = pd.read_csv('expat_dakar_appart_alouer.csv')
-appart_meuble = pd.read_csv('expat_dakar_appart_meuble.csv')
-terrain_a_vendre = pd.read_csv('expat_dakar_terrain_a_vendre.csv')
-
-
-# st.bar_chart(data=appart_louer, x=None, y=None, color=None, width=0, height=0, use_container_width=True)
 
 # Fonction Background
 def add_bg_from_local(image_file):
@@ -54,9 +27,63 @@ def add_bg_from_local(image_file):
 
 add_bg_from_local('background.jpg')
 
+# Page Principale
+st.title("Projet de Data Collection Groupe 5")
+st.write(
+    "This app performs webscraping of data from expat-dakar over multiples pages. "
+    "And we can also download scraped data from the app directly without scraping them.")
+st.markdown("- Python libraries: base64, pandas, streamlit, requests, bs4")
+st.markdown("- Data source: at-Dakar.")
 
-def scraper_page():
-    """
-    Fonction qui permet de scraper de les les pages avec BeautifulSoup
-    :return: Pandas DataFrame
-    """
+# Barre de Menu lattérale
+st.sidebar.title("Menu")
+
+# création d'une liste de 1 à 100
+list1 = [i for i in range(1, 101)]
+
+option1 = st.sidebar.selectbox(
+    "Pages indexes",
+    list1,
+)
+
+option2 = st.sidebar.selectbox(
+    "Options",
+    ("Scrape data using beautifulSoup", "Download scraped data", "Dashbord of the data", "Fill the form"),
+    index=None
+)
+
+
+def load(dataframe, title, key, key1):
+    st.markdown("""
+    <style>
+    div.stButton {text-align:center}
+    </style>""", unsafe_allow_html=True)
+
+    if st.button(title, key1):
+        # st.header(title)
+
+        st.subheader('Display data dimension')
+        st.write('Data dimension: ' + str(dataframe.shape[0]) + ' rows and ' + str(dataframe.shape[1]) + ' columns.')
+        st.dataframe(dataframe)
+
+        csv = dataframe.to_csv().encode('utf-8')
+
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='Data.csv',
+            mime='text/csv',
+            key=key)
+
+
+if option2 == "Scrape data using beautifulSoup":
+    appart_a_louer = scrap_appart_a_louer(option1)
+    appart_meuble = scrap_appart_meuble(option1)
+    terrain_a_vendre = srappe_terrain_a_vendre(option1)
+
+    load(appart_a_louer, 'Appartements à louer', '1', '101')
+    load(appart_meuble, 'Appartement meublés', '2', '102')
+    load(terrain_a_vendre, 'Terrains à vendre', '3', '103')
+
+# load(Motocycles, 'Motocycles data', '2', '102')
+# st.bar_chart(data=appart_louer, x=None, y=None, color=None, width=0, height=0, use_container_width=True)
